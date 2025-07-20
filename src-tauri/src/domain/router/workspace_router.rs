d5ed13cfd77a3d16947501df25e2a0fb67bb9235
masrayfa <maselon2030@gmail.com>
2025-07-20 19:46:42 +0700

    feat: Implement workspace router with CRUD operations for workspaces

--- src-tauri/src/domain/router/workspace_router.rs
+++ src-tauri/src/domain/router/workspace_router.rs
@@ -1,10 +1,55 @@
-use crate::router::ContextRouter;
-use rspc::{Router, RouterBuilder};
+use std::sync::Arc;
+
+use crate::{
+    domain::{
+        dto::workspace::workspace_dto::{CreateWorkspaceDto, UpdateWorkspaceDto},
+        repository::workspace_repository::WorkspaceRepositoryImpl,
+        service::workspace_service::{WorkspaceService, WorkspaceServiceImpl},
+    },
+    router::ContextRouter,
+};
+use rspc::{ErrorCode, Router, RouterBuilder};
 
 pub fn create_workspace_router() -> RouterBuilder<ContextRouter> {
     Router::<ContextRouter>::new()
-        .query("list", |t| {
-            return t(|_ctx, _input: ()| async move { vec![] as Vec<()> });
+        .query("getWorkspaces", |t| {
+            t.resolver(|ctx: ContextRouter, _input: ()| async move {
+                let repo = Arc::new(WorkspaceRepositoryImpl::new());
+                let service = WorkspaceServiceImpl::new(repo);
+                service
+                    .list_workspace(ctx)
+                    .await
+                    .map_err(|e| rspc::Error::new(ErrorCode::InternalServerError, e))
+            })
         })
-        .query("get", |t| t(|_ctx, id: u32| format!("User {}", id)))
+        .mutation("createWorkspace", |t| {
+            t.resolver(|ctx: ContextRouter, input: CreateWorkspaceDto| async move {
+                let repo = Arc::new(WorkspaceRepositoryImpl::new());
+                let service = WorkspaceServiceImpl::new(repo);
+                service
+                    .create_workspace(ctx, input)
+                    .await
+                    .map_err(|e| rspc::Error::new(ErrorCode::InternalServerError, e))
+            })
+        })
+        .mutation("updateWorkspace", |t| {
+            t.resolver(|ctx: ContextRouter, input: UpdateWorkspaceDto| async move {
+                let repo = Arc::new(WorkspaceRepositoryImpl::new());
+                let service = WorkspaceServiceImpl::new(repo);
+                service
+                    .update_workspace(ctx, input)
+                    .await
+                    .map_err(|e| rspc::Error::new(ErrorCode::InternalServerError, e))
+            })
+        })
+        .mutation("deleteWorkspace", |t| {
+            t.resolver(|ctx: ContextRouter, input: i32| async move {
+                let repo = Arc::new(WorkspaceRepositoryImpl::new());
+                let service = WorkspaceServiceImpl::new(repo);
+                service
+                    .delete_workspace(ctx, input)
+                    .await
+                    .map_err(|e| rspc::Error::new(ErrorCode::InternalServerError, e))
+            })
+        })
 }
